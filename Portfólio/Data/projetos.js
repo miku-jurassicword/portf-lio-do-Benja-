88de67a8b9d8902828506fb37d62b70cf1977512
miku-jurassicword <benjamimlm13@gmail.com>
2026-06-20 13:15:39 -0300

    Update projetos.js

--- Portfólio/Data/projetos.js
+++ Portfólio/Data/projetos.js
@@ -1,1 +1,23 @@
 
+const projetos = [
+{
+    nome: "Eco Valley",
+    descricao: "Jogo de farming sustentável.",
+    imagem: "Img/Eco-valley.png",
+    arquivo: "Projetos/eco-valley/index.html"
+},
+
+{
+    nome: "Greenvale",
+    descricao: "Projeto sobre saúde e bem-estar.",
+    imagem: "Img/Eco-valley.png",
+    arquivo: "Projetos/greenvale/index.html"
+},
+
+{
+    nome: "Radar Saúde",
+    descricao: "Mapa e consulta de hospitais.",
+    imagem: "Img/Eco-valley.png",
+    arquivo: "Projetos/radar-saude/Radar_saude_completo.html"
+}
+];
