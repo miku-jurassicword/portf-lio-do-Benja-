09bea48079a71da9cfba02147b02e66b3e1703d1
miku-jurassicword <benjamimlm13@gmail.com>
2026-06-20 15:21:16 -0300

    Update script.js

--- Portfólio/script.js
+++ Portfólio/script.js
@@ -1,30 +1,111 @@
 const cards = document.getElementById("cards");
 
-projetos.forEach(projeto => {
+const search = document.getElementById("search");
 
-    const div = document.createElement("div");
+const buttons =
+document.querySelectorAll("[data-filter]");
 
-    div.className = "project";
+let filtro = "all";
 
-    div.innerHTML = `
-    
-        <div class="project-info">
-            <h3>${projeto.nome}</h3>
-            <p>${projeto.descricao}</p>
-        </div>
+function renderProjetos(){
 
-        <button class="open">
-            Abrir →
-        </button>
+cards.innerHTML = "";
 
-    `;
+const texto =
+search.value.toLowerCase();
 
-    div.querySelector(".open").onclick = () => {
+const filtrados =
+projetos.filter(projeto => {
 
-        window.location.href = projeto.arquivo;
+const nome =
+projeto.nome.toLowerCase();
 
-    };
+const busca =
+nome.includes(texto);
 
-    cards.appendChild(div);
+const categoria =
+filtro === "all" ||
+projeto.categoria === filtro ||
+projeto.linguagem === filtro;
+
+return busca && categoria;
 
 });
+
+filtrados.forEach(projeto => {
+
+const div =
+document.createElement("div");
+
+div.className = "project";
+
+div.innerHTML = `
+
+<div class="project-info">
+
+<h3>${projeto.nome}</h3>
+
+<p>${projeto.descricao}</p>
+
+<div class="tags">
+
+<span>${projeto.categoria}</span>
+<span>${projeto.linguagem}</span>
+
+</div>
+
+</div>
+
+<button class="open">
+Abrir →
+</button>
+
+`;
+
+div
+.querySelector(".open")
+.onclick = () => {
+
+window.location.href =
+projeto.arquivo;
+
+};
+
+cards.appendChild(div);
+
+});
+
+}
+
+search.addEventListener(
+"input",
+renderProjetos
+);
+
+buttons.forEach(btn => {
+
+btn.onclick = () => {
+
+filtro =
+btn.dataset.filter;
+
+renderProjetos();
+
+};
+
+});
+
+renderProjetos();
+
+const sidebar =
+document.querySelector(".sidebar");
+
+document
+.getElementById("toggleSidebar")
+.onclick = () => {
+
+sidebar.classList.toggle(
+"collapsed"
+);
+
+};
