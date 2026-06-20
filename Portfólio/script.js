ac9122c1af7c582ed2c3c9a8a5c74506218b40bf
miku-jurassicword <benjamimlm13@gmail.com>
2026-06-20 13:20:40 -0300

    Update script.js

--- Portfólio/script.js
+++ Portfólio/script.js
@@ -1,1 +1,50 @@
+const cards = document.getElementById("cards");
 
+const modal = document.getElementById("modal");
+const frame = document.getElementById("projectFrame");
+const closeModal = document.getElementById("closeModal");
+
+projetos.forEach(projeto => {
+
+const card = document.createElement("div");
+
+card.className = "card";
+
+card.innerHTML = `
+<img src="${projeto.imagem}">
+<div class="card-content">
+
+<h3>${projeto.nome}</h3>
+
+<p>${projeto.descricao}</p>
+
+<div class="tags">
+<span>HTML</span>
+<span>CSS</span>
+<span>JS</span>
+</div>
+
+<button>Abrir Projeto</button>
+
+</div>
+`;
+
+card.querySelector("button").onclick = () => {
+
+modal.classList.add("active");
+
+frame.src = projeto.arquivo;
+
+};
+
+cards.appendChild(card);
+
+});
+
+closeModal.onclick = () => {
+
+modal.classList.remove("active");
+
+frame.src = "";
+
+};
